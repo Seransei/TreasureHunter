77f23ec82fef8ce6975b0ad12385ca58cb540558
TheWincher <louchez26@hotmail.fr>
2019-11-13 18:20:55 +0100

    Ajout du déplacement lattéral et du jump avec la manette

---
+++ TreasureHunters/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float heightJump = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+
+        direction.x = Input.GetAxis("Horizontal") * speed;
+        //direction.y = Input.GetAxis("Vertical");
+
+        if (Input.GetButton("A_Button"))
+            direction += Vector3.up * heightJump;
+
+        gameObject.transform.position = gameObject.transform.position + direction * Time.deltaTime;
+       
+    }
+}
